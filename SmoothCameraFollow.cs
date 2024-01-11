d8a2b5e3d5d12ba64b464a1523b0e8c5818e4322
Stephen Pasch <84539220+ChefSteveP@users.noreply.github.com>
2024-01-11 03:16:34 -0500

    added left/right stops for camera to not follow player off edges

--- SmoothCameraFollow.cs
+++ SmoothCameraFollow.cs
@@ -9,10 +9,25 @@ public class SmoothCameraFollow : MonoBehaviour
     public float horDamping;
     public float vertDamping;
     private Vector3 velocity = Vector3.zero;
+
+    [SerializeField]
+    private float leftStop = -Mathf.Infinity;
+    [SerializeField]
+    private float rightStop = Mathf.Infinity;
     // Update is called once per frame
     void FixedUpdate()
     {
+        //Calculate the camera's position based on the player's position
         Vector3 movePosition = player.position + offset;
+
+        //if player moves past leftStop, camera stops moving left
+        if(player.position.x < leftStop){
+            movePosition.x = leftStop;
+        }
+        //if player moves past rightStop, camera stops moving right
+        if(player.position.x > rightStop){
+            movePosition.x = rightStop;
+        }
 
         //Horizontal Dampening
         transform.position = Vector3.SmoothDamp(transform.position,new Vector3(movePosition.x, transform.position.y,movePosition.z), ref velocity, horDamping);
